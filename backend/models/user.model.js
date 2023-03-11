7f141e2ed14d767d2510334d93689267744cbb94
omjiverma <omjidvlpr@gmail.com>
2023-03-11 09:18:08 +0530

    Email validation

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require('validator')
 
 const UserSchema = new mongoose.Schema({
   firstName: {
@@ -16,6 +17,10 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provide email"],
+    validate:{
+      validator:validator.isEmail,
+      message: 'Please provide valid email'
+    },
   },
   password: {
     type: String,
